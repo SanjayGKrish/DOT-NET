1e8639cac8d8dd23674e645b38655dcea36174a7
SanjayGKrish <87979163+SanjayGKrish@users.noreply.github.com>
2021-08-11 22:10:45 +0530

    Add files via upload

---
+++ GotoDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVeryFirstApp
+{
+    class GotoDemo
+    {
+        int age = 17;
+
+            if(age > 18)
+            {
+                goto BLOCK1
+    }
+            else
+            {
+                goto EXIT1
+}
+
+label BLOCK1;
+Console.WriteLine("hi aliens");
+label EXIT1;
+Console.WriteLine("Get out of my teritory");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVeryFirstApp
+{
+    class IfDemo
+    {
+        static void Main(string[] args)
+
+        {
+            #region if demo
+            /* if (1<3)
+             {
+                 Console.WriteLine("sanjay is winner");
+             }
+             else
+             {
+                 Console.WriteLine("sanjay is always winner");
+             } */
+            #endregion
+            #region switch
+            /*
+            int a = 3;
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+                case 2:
+                    Console.WriteLine("two");
+                    break;
+                case 3:
+                    Console.WriteLine("three");
+                    break;
+                default:
+                    Console.WriteLine("not possible");
+                    break;
+            }
+            */
+            #endregion
+            #region goto
+            
+            #endregion
+        }
+
+
+    }
+}
